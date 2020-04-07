dfe5fa0dec29aab8f74cac43f2241b49e415069e
angelLeoLim <63270297+angelLeoLim@users.noreply.github.com>
2020-04-06 23:12:29 -0500

    Update consulta_1.php

--- consulta_1.php
+++ consulta_1.php
@@ -12,7 +12,7 @@
 		$sql="INSERT INTO tbcompras ( IDproveedor, Producto, Precio, Cantidad, Fecha, Precio_total) VALUES ( '$IDproveedor', '$Producto', '$Precio', '$Cantidad','$Fecha','$Precio_total')";
 		$result=$mysqli->query($sql);
 		
-		$variableSinUsar
+		sfsihfsihfishisf
 
 
 		//Aqui se empieza a modificar la tabla de existencias
